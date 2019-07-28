5e982e41fa87f2e6cb3636b09fd12bc55f7bc8b2
unknown <javigner@student.42.fr>
2019-07-28 19:12:23 +0200

    Sigmoid and prediction Done

--- logistic_regression.py
+++ logistic_regression.py
@@ -22,13 +22,9 @@ def gradient_descent(theta, X, y):
     learning_rate = 0.1
     print(theta[0], theta[1], theta[2])
     while (testha.all() != theta.all()):
-        theta[0] = testha[0]
-        theta[1] = testha[1]
-        theta[2] = testha[2]
-        testha[0] = theta[0] - learning_rate * (1.0 / len(X)) * (np.dot(predict(X, theta) - y, X))
-        testha[1] = theta[1] - learning_rate * (1.0 / len(X)) * (np.dot(predict(X, theta) - y, X))
-        testha[2] = theta[2] - learning_rate * (1.0 / len(X)) * (np.dot(predict(X, theta) - y, X))
-    return theta, mean, stdev
+        theta = testha
+        testha = theta - learning_rate * (1.0 / len(X)) * (np.dot(predict(X, theta) - y, X))
+    return theta, mean, stdev, X
     
 
 def main():
@@ -38,12 +34,21 @@ def main():
     X = np.array(X)
     y = np.array(y)
     theta = np.zeros(3)
-    theta, mean, stdev = gradient_descent(theta, X, y)
+    theta, mean, stdev, X = gradient_descent(theta, X, y)
     X_test = (np.array([34,78]) - mean) / stdev
     X_test = np.hstack([1, X_test])
-    print(predict(X_test, theta))
-    
-
+    new_pred = predict(X, theta)
+    for i in range(len(new_pred)):
+        if new_pred[i] >= 0.5:
+            new_pred[i] = 1
+        else:
+            new_pred[i] = 0
+    Accuracy = 0
+    for z in range(len(new_pred)):
+        print(new_pred[z], y[z])
+        if new_pred[z] == y[z]:
+            Accuracy += 1
+    print(Accuracy / len(new_pred))
 if __name__ == "__main__":
     main();
 
